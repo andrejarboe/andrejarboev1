75dc1e2e5c7c67fcbb5f827780c36855a5ab46a1
andrejarboe <andrejarboe@gmail.com>
2018-09-16 15:51:25 -0400

    start css grid

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import '../css/home.css'
+
 const IndexPage = () => (
   <div>
     <div id="container">
-      
+      <div>
+        <img src="https://source.unsplash.com/user/erondu/900x1600" alt="" />
+      </div>
+      <div>
+        <img src="https://source.unsplash.com/user/erondu/900x1600" alt="" />
+      </div>
+      <div>
+        <img src="https://source.unsplash.com/user/erondu/900x1600" alt="" />
+      </div>
+      <div>
+        <img src="https://source.unsplash.com/user/erondu/900x1600" alt="" />
+      </div>
     </div>
   </div>
 )
