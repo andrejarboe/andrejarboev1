acc1f367438b16fd10d793f02d207f2dfdcfc045
andrejarboe <andrejarboe@gmail.com>
2018-09-19 19:54:36 -0400

    deleated extra div on home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-
 const IndexPage = () => (
-  <div>
-    <div id="home">
-      <div>
-        <img src="https://source.unsplash.com/user/erondu/900x1600" alt="" />
-      </div>
-      <div>
-        <img src="https://source.unsplash.com/user/erondu/900x1601" alt="" />
-      </div>
-      <div>
-        <img src="https://source.unsplash.com/user/erondu/900x1602" alt="" />
-      </div>
+  <div id="home">
+    <div>
+      <img src="https://source.unsplash.com/user/erondu/900x1600" alt="" />
+    </div>
+    <div>
+      <img src="https://source.unsplash.com/user/erondu/900x1601" alt="" />
+    </div>
+    <div>
+      <img src="https://source.unsplash.com/user/erondu/900x1602" alt="" />
     </div>
   </div>
-)    
+)
 
 export default IndexPage
