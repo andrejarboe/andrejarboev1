ca85fa7df846c2bc07c767e4c217c8ea081d680f
andrejarboe <andrejarboe@gmail.com>
2018-09-21 15:45:59 -0400

    add home templete

--- src/templates/home.jsx
+++ src/templates/home.jsx
@@ -1,20 +1,4 @@
 import React from 'react'
-import Link from 'gatsby-link'
-
-// const IndexPage = ({data}) => (
-
-//   <div id="home">
-//     <div>
-//       <img src="https://source.unsplash.com/user/erondu/900x1600" alt="" />
-//     </div>
-//     <div>
-//       <img src="https://source.unsplash.com/user/erondu/901x1601" alt="" />
-//     </div>
-//     <div>
-//       <img src="https://source.unsplash.com/user/erondu/902x1602" alt="" />
-//     </div>
-//   </div>
-// )
 
 export default function IndexPage({ data }) {
   const post = data.markdownRemark
@@ -33,8 +17,6 @@ export default function IndexPage({ data }) {
     </div>
   )
 }
-
-// export default IndexPage
 
 export const query = graphql`
   query homeQuery {
