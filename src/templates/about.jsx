af7d34c7db75bdfb0c325e45db11ff6251b13ab6
andrejarboe <andrejarboe@gmail.com>
2018-09-23 15:58:15 -0400

    working on home page collection

--- src/templates/about.jsx
+++ src/templates/about.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 export default function Template({ data }) {
-  const post = data.markdownRemark
+  const about = data.markdownRemark
 
   return (
     <div>
       <Link to="/about">Back to Blog Post</Link>
       <hr />
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <h1>{about.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: about.html }} />
     </div>
   )
 }
