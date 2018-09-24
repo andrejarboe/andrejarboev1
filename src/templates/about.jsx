c6b888c47cc1aa166d9e14adbe3a4eb02da95072
andrejarboe <andrejarboe@gmail.com>
2018-09-24 15:40:43 -0400

    start about page css

--- src/templates/about.jsx
+++ src/templates/about.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import Headshot1 from '../../static/assets/headshot1.jpg'
 
 export default function Template({ data }) {
   const about = data.markdownRemark
 
   return (
-    <div>
-      <Link to="/about">Back to Blog Post</Link>
-      <hr />
-      <h1>{about.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: about.html }} />
+    <div id="about">
+      <div className="container">
+        <div className="headshot">
+          <img src={Headshot1} alt="" />
+        </div>
+        <div className="content">
+          <h1>{about.frontmatter.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: about.html }} />
+          <a href="/resume" className="btn">See Resume</a>
+        </div>
+      </div>
     </div>
   )
 }
